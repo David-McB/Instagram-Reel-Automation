92ccf182132fa96343f1d2a3d63fe6ea14642082
David McBride <dnmcbride@live.co.uk>
2024-10-06 11:54:55 +0100

    fix: improve reel render process and fix bugs

--- src/editor.ts
+++ src/editor.ts
@@ -1,21 +1,28 @@
 import ffmpeg from 'fluent-ffmpeg';
+import ffmpegInstall from '@ffmpeg-installer/ffmpeg';
+import ffProbeInstall from '@ffprobe-installer/ffprobe';
 import open from 'open';
 import { promises as fs } from 'fs';
 import type { EditorOptions } from './types/editorOptions';
 import { INSTAGRAM_ASPECT_RATIO, VIDEO_PATH, AUDIO_PATH, TRIMMED_AUDIO_PATH, SRT_SAVE_LOCATION, REEL_DIRECTORY, SUBTITLE_API_URL } from './constants.js';
 import path from 'path';
 
+ffmpeg.setFfmpegPath(ffmpegInstall.path);
+ffmpeg.setFfprobePath(ffProbeInstall.path);
+
 class Editor {
     private savedReelLocation: string;
     private startTimeSeconds: number;
     private endTimeSeconds: number;
     private reelDuration: number;
+    private originalDuration: number;
 
     constructor(options: EditorOptions) {
         this.savedReelLocation = options.savedReelLocation;
         this.startTimeSeconds = options.trimTimestamp.start;
         this.endTimeSeconds = options.trimTimestamp.end;
         this.reelDuration = this.endTimeSeconds - this.startTimeSeconds;
+        this.originalDuration = 0;
     }
 
     /** Finds maximum possible size for a video for a given aspect ratio */
@@ -26,6 +33,7 @@ class Editor {
                 const [targetWidth, targetHeight] = aspectRatio.split(':');
                 const videoHeight = metadata.streams[0].height;
                 const videoWidth = metadata.streams[0].width;
+                this.originalDuration = Number(metadata.streams[0].duration);
     
                 if (!videoHeight || !videoWidth) reject('Video width/height could not be found');
     
@@ -78,6 +86,8 @@ class Editor {
         const logoWidth = width > 500 ? 220 : 100; // Logo may not be centered for resolutions < 1080p
 
         // Trim audio first before generating subtitles to ensure it stays in sync
+        console.log('Trimming reel audio. Please wait...')
+
         await new Promise<void>((resolve, reject) => {
             ffmpeg(AUDIO_PATH)
             .audioFilters([
@@ -87,8 +97,6 @@ class Editor {
             .output(TRIMMED_AUDIO_PATH)
             .on('progress', progress => {
                 if (!progress.percent) return;
-                //TODO: fix progress bar
-                console.log(`Trimming reel audio: ${Math.floor(progress.percent)}% done`);
             })
             .on('error', error => {
                 console.log(`Unable to processs audio: ${error.message}`);
@@ -110,11 +118,11 @@ class Editor {
             {filter: 'color', options: {color: 'black@.4', size: `${+width}x${+height}`, duration: this.reelDuration}, outputs: 'overlay'},
             {filter: 'crop', options: {w: width, h: height}, inputs: '0:v', outputs: 'croppedReel'},
             {filter: 'trim', options: {start: this.startTimeSeconds, end: this.endTimeSeconds }, inputs: 'croppedReel', outputs: 'trimmedReel'},
-            {filter: 'setpts', options: 'PTS-STARTPTS', inputs: 'trimmedReel', outputs: 'newTrimmedReel'}, // problem here
+            {filter: 'setpts', options: 'PTS-STARTPTS', inputs: 'trimmedReel', outputs: 'newTrimmedReel'},
             {filter: 'overlay', options: {x: 0, y: 0}, inputs: ['newTrimmedReel', 'overlay'], outputs: 'darkenedReel'},
             {filter: 'scale', options: {w: '157.5', h: '118.1'}, inputs: '2:v', outputs: 'scaledLogo'},
-            {filter: 'overlay', options: {x: logoWidth, y: 130}, inputs: ['darkenedReel', 'scaledLogo'], outputs: 'reelWithLogo'},
-            {filter: 'subtitles', options: {filename: SRT_SAVE_LOCATION + '/subtitles.srt', force_style: "Alignment=10,FontName=Avenir Next Bold,Fontsize=12,MarginL=5,MarginV=25,Outline=0"}, inputs: 'reelWithLogo'}
+            {filter: 'overlay', options: {x: logoWidth, y: 130, format: 'auto'}, inputs: ['darkenedReel', 'scaledLogo'], outputs: 'reelWithLogo'},
+            {filter: 'subtitles', options: {filename: SRT_SAVE_LOCATION + '/subtitles.srt', force_style: "Alignment=10,FontName=Avenir Next Bold,Fontsize=8,MarginL=5,MarginV=25,Outline=0"}, inputs: 'reelWithLogo'}
         ])
         .audioFilters([
             {filter: 'atrim', options: {start: this.startTimeSeconds, end: this.endTimeSeconds}},
@@ -126,8 +134,11 @@ class Editor {
         .videoBitrate(15000)
         .on('progress', progress => {
             if (!progress.percent) return;
-            //TODO: fix progress bar
-            console.log(`Processing reel: ${Math.floor(progress.percent)}% done`);
+            const [ hours, minutes ] = progress.timemark.split(':');
+            const seconds = Math.round(+progress.timemark.split(":")[2]);
+            const totalSeconds = +hours * 60 * 60 + (+minutes) * 60 + seconds;
+            const progressPercentage = (totalSeconds / this.reelDuration) * 100;
+            console.log(`Processing reel: ${Math.floor(progressPercentage)}% done`);
         })
         .on('error', error => console.log(`Unable to process video: ${error.message}`))
         .on('end', async () => {
--- src/init.ts
+++ src/init.ts
@@ -6,64 +6,64 @@ import 'dotenv/config';
 
 const youtubeRegex = /^(https?\:\/\/)?((www\.)?youtube\.com|youtu\.be)\/.+$/;
 if (process.env.DEVELOPMENT === 'true') initiateImmediateDownload();
-else {
-    (async () => {
-        const response = await prompts([
-            {
-                type: 'text',
-                name: 'url',
-                message: "Please paste a YouTube download URL and press 'enter'",
-                validate: value => youtubeRegex.test(value) ? true : 'Please enter a link to a valid YouTube video'
-            },
-            {
-                type: 'number',
-                name: 'start',
-                message: "Please enter the time in seconds you would like to start the reel from. Enter '0' to start the reel from the beginning of the video"
-            }
-        ]);
-
-        const { url, start } = response;
-
-        const endResponse = await prompts({
-            type: 'number',
-            name: 'end',
-            message: 'Please enter the time in seconds you would like to end the reel at',
-            validate: value => value - start <= 90 ? true : 'Total reel duration must be 90 seconds or less'
-        })
-
-        const end = endResponse.end;
-
-        const options: EditorOptions = {
-            savedReelLocation: './convertedReel.mp4',
-            trimTimestamp: { start, end }
-        };
-
-        const editor = new Editor(options);
-        
-        try {
-            await download(url);
-            console.log('\nDownload completed. Creating reel...\n');
-            await editor.createReel();
-        }
-
-        catch(error: any) {
-            throw new Error(error);
-        }
-    })();
-    }
+else init();
 
 async function initiateImmediateDownload() {
     // await Editor.verifyReelSaveLocation();
 
     const editor = new Editor({
         savedReelLocation: './convertedReel.mp4',
-        trimTimestamp: {start: 3, end: 20}
+        trimTimestamp: {start: 20, end: 100}
     });
 
-    const URL = "https://www.youtube.com/watch?v=3YWIIV19U70&t=4s";
+    const URL = "https://www.youtube.com/watch?v=ljqra3BcqWM";
 
     try {
         await download(URL);
+        console.log('\nDownload completed. Creating reel...\n');
+        await editor.createReel();
+    }
+
+    catch(error: any) {
+        throw new Error(error);
+    }
+}
+
+async function init() {
+    const response = await prompts([
+        {
+            type: 'text',
+            name: 'url',
+            message: "Please paste a YouTube download URL and press 'enter'",
+            validate: value => youtubeRegex.test(value) ? true : 'Please enter a link to a valid YouTube video'
+        },
+        {
+            type: 'number',
+            name: 'start',
+            message: "Please enter the time in seconds you would like to start the reel from. Enter '0' to start the reel from the beginning of the video"
+        }
+    ]);
+
+    const { url, start } = response;
+
+    const endResponse = await prompts({
+        type: 'number',
+        name: 'end',
+        message: 'Please enter the time in seconds you would like to end the reel at',
+        validate: value => value - start <= 90 ? true : 'Total reel duration must be 90 seconds or less'
+    })
+
+    const end = endResponse.end;
+
+    const options: EditorOptions = {
+        savedReelLocation: './convertedReel.mp4',
+        trimTimestamp: { start, end }
+    };
+
+    const editor = new Editor(options);
+    
+    try {
+        await download(url);
         console.log('\nDownload completed. Creating reel...\n');
         await editor.createReel();
     }
