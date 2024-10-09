13326d0fa8b41ad98268f86ea661fb17c3c896f0
David McBride <dnmcbride@live.co.uk>
2024-10-09 19:09:45 +0100

    fix: verify reel save directory

--- src/init.ts
+++ src/init.ts
@@ -67,7 +67,7 @@ async function init() {
     const [ startSeconds, endSeconds ] = convertStartAndEndTimes(start, end);
 
     const options: EditorOptions = {
-        savedReelLocation: './',
+        savedReelLocation: REEL_SAVE_DIRECTORY,
         trimTimestamp: { start: startSeconds, end: endSeconds }
     };
 
@@ -77,6 +77,7 @@ async function init() {
         const info = await download(url);
         editor.setVideoName(info[2].videoDetails.title);
         console.log('\nDownload completed. Creating reel...\n');
+        await Editor.verifyReelSaveLocation();
         await editor.createReel();
     }
 
